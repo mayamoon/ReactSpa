e0d1798f81223223086b7ccbbec01226d0c9902f
mayamoon <mayamoon@163.com>
2016-11-20 23:53:58 +0800

    add

---
+++ ClientApp/components/Counter.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Link } from 'react-router';
+import { provide } from 'redux-typed';
+import { ApplicationState }  from '../store';
+import * as CounterStore from '../store/Counter';
+var $ = require("jquery") as any;
+
+class Counter extends React.Component<CounterProps, void> {
+    public componentDidMount() {
+        $(ReactDOM.findDOMNode(this)).text("adfsf");
+    }
+    public render() {
+        return <div>
+            <h1>Counter</h1>
+
+            <p>This is a simple example of a React component.</p>
+
+            <p>Current count: <strong>{ this.props.count }</strong></p>
+
+            <button onClick={ () => { this.props.increment() } }>Increment</button>
+        </div>;
+    }
+}
+
+// Build the CounterProps type, which allows the component to be strongly typed
+const provider = provide(
+    (state: ApplicationState) => state.counter, // Select which part of global state maps to this component
+    CounterStore.actionCreators                 // Select which action creators should be exposed to this component
+);
+type CounterProps = typeof provider.allProps;
+export default provider.connect(Counter);
---
+++ ClientApp/components/ReactJQueryUI.ts
@@ -0,0 +1,63 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+var $ = require("jquery") as any;
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+var shallowCompare = require('react/lib/shallowCompare') as any;
+
+function wrapWidget(name:string) {
+    var displayName = 'React' + name[0].toUpperCase() + name.slice(1);
+
+    return React.createClass({
+        render: function () {
+            return this.props.children;
+        },
+
+        componentDidUpdate: function (prevProps) {
+            if (!shallowCompare(prevProps, this.props)) {
+                this._runPlugin();
+            }
+        },
+
+        componentDidMount: function () {
+            this._runPlugin();
+        },
+
+        _runPlugin: function () {
+            var $node = $(ReactDOM.findDOMNode(this));
+            $node[name](this.props);
+            this.$ = $node;
+        },
+
+        displayName: displayName
+    });
+}
+
+var WIDGETS = {
+    Accordion: 'accordion',
+    Autocomplete: 'autocomplete',
+    Button: 'button',
+    DatePicker: 'datepicker',
+    Draggable: 'draggable',
+    Droppable: 'droppable',
+    Menu: 'menu',
+    ProgressBar: 'progressbar',
+    Resizable: 'resizable',
+    Selectable: 'selectable',
+    Sortable: 'sortable',
+    Slider: 'slider',
+    Spinner: 'spinner',
+    Tabs: 'tabs',
+    Tooltip: 'tooltip',
+    Dialog:"dialog"
+};
+
+var ReactJQueryUI: { [name: string]: React.ClassicComponentClass<any> } = {};
+
+for (var key in WIDGETS) {
+    ReactJQueryUI[key] = wrapWidget(WIDGETS[key]);
+}
+
+export default ReactJQueryUI;
+
